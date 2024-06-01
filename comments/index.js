23ff5ee0fcce494ef48b7bba24487e82e74e9e9c
tuwnguyen <tuwnguyen@gmail.com>
2024-06-01 15:49:49 +0700

    imp the client, comments, posts, query, event bus services

---
+++ comments/index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
+const bodyParser = require('body-parser');
+const { randomBytes } = require('crypto');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+const commentsByPostId = {};
+
+app.get('/posts/:id/comments', (req, res) => {
+  res.send(commentsByPostId[req.params.id] || []);
+});
+
+app.post('/posts/:id/comments', async (req, res) => {
+  const commentId = randomBytes(4).toString('hex');
+  const { content } = req.body;
+  const comments = commentsByPostId[req.params.id] || [];
+  comments.push({ id: commentId, content });
+  commentsByPostId[req.params.id] = comments;
+
+  await axios.post('http://localhost:4005/events', {
+    type: 'CommentCreated',
+    data: {
+      id: commentId,
+      content,
+      postId: req.params.id,
+    },
+  });
+
+  res.status(201).send(comments);
+});
+
+app.post('/events', (req, res) => {
+  // console.log('Event received', req.body);
+  res.send({});
+});
+
+app.listen(4001, () => {
+  console.log('listening on port 4001');
+});
