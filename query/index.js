8f189760bd675a01903801e9219b153c37c8c1e0
tuwnguyen <tuwnguyen@gmail.com>
2024-06-05 00:06:03 +0700

    imp Event Sync when dealing with Missing Events

--- query/index.js
+++ query/index.js
@@ -9,24 +9,42 @@ app.use(bodyParser.json());
 
 const posts = {};
 
-app.post('/events', (req, res) => {
-  const { type, data } = req.body;
+const handleEvent = (type, data) => {
   if (type === 'PostCreated') {
     const { id, title } = data;
     posts[id] = { id, title, comments: [] };
   }
   if (type === 'CommentCreated') {
-    const { id, content, postId } = data;
+    const { id, content, postId, status } = data;
     const post = posts[postId];
-    post.comments.push({ id, content });
+    post.comments.push({ id, content, status });
   }
-  console.log(':::here', posts);
+
+  if (type === 'CommentUpdated') {
+    const { id, content, postId, status } = data;
+    const post = posts[postId];
+    const comment = post.comments.find((c) => c.id === id);
+    comment.status = status;
+    comment.content = content;
+  }
+};
+
+app.post('/events', (req, res) => {
+  const { type, data } = req.body;
+  handleEvent(type, data);
+  res.send({});
 });
 
 app.get('/posts', (req, res) => {
   res.send(posts);
 });
 
-app.listen(4002, () => {
+app.listen(4002, async () => {
   console.log('listening on 4002');
+  const res = await axios.get('http://localhost:4005/events');
+
+  for (let event of res.data) {
+    console.log('Processing event:', event);
+    handleEvent(event.type, event.data);
+  }
 });
