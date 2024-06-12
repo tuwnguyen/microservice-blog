533f32e3da408623908d3d31d3fb9c19ec3fe791
tuwnguyen <tuwnguyen@gmail.com>
2024-06-12 22:49:21 +0700

    create Deployment of Client service, change host from localhost to posts.com

--- client/src/PostList.js
+++ client/src/PostList.js
@@ -8,7 +8,7 @@ const PostList = () => {
 
   useEffect(() => {
     const fetchPosts = async () => {
-      const res = await axios.get('http://localhost:4002/posts');
+      const res = await axios.get('http://posts.com/posts');
       console.log(res.data);
       setPosts(res.data);
     };
